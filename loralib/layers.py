3c2cd48db4d1abc73f1d34498d3b49e3ff537ae2
Burak Soner <buraksoner92@gmail.com>
2024-08-13 13:39:06 +0300

    trying to remove the conv module

--- loralib/layers.py
+++ loralib/layers.py
@@ -246,25 +246,31 @@ class MergedLinear(nn.Linear, LoRALayer):
 class ConvLoRA(nn.Module, LoRALayer):
     def __init__(self, conv_module, in_channels, out_channels, kernel_size, r=0, lora_alpha=1, lora_dropout=0., merge_weights=True, **kwargs):
         super(ConvLoRA, self).__init__()
-        self.conv = conv_module(in_channels, out_channels, kernel_size, **kwargs)
+        nn.Conv2d.__init__(self, in_channels, out_channels, kernel_size, **kwargs)
+        #self.conv = conv_module(in_channels, out_channels, kernel_size, **kwargs)
         LoRALayer.__init__(self, r=r, lora_alpha=lora_alpha, lora_dropout=lora_dropout, merge_weights=merge_weights)
         assert isinstance(kernel_size, int)
         # Actual trainable parameters
         if r > 0:
             self.lora_A = nn.Parameter(
-                self.conv.weight.new_zeros((r * kernel_size, in_channels * kernel_size))
+                self.weight.new_zeros((r * kernel_size, in_channels * kernel_size))
+                #self.conv.weight.new_zeros((r * kernel_size, in_channels * kernel_size))
             )
             self.lora_B = nn.Parameter(
-              self.conv.weight.new_zeros((out_channels//self.conv.groups*kernel_size, r*kernel_size))
+                self.weight.new_zeros((out_channels//self.groups*kernel_size, r*kernel_size))
+                #self.conv.weight.new_zeros((out_channels//self.conv.groups*kernel_size, r*kernel_size))
             )
             self.scaling = self.lora_alpha / self.r
             # Freezing the pre-trained weight matrix
-            self.conv.weight.requires_grad = False
+            self.weight.requires_grad = False
+            #self.conv.weight.requires_grad = False
         self.reset_parameters()
         self.merged = False
 
     def reset_parameters(self):
-        self.conv.reset_parameters()
+        #self.conv.reset_parameters()
+        nn.init.zeros_(self.weight)
+        nn.init.zeros_(self.bias)
         if hasattr(self, 'lora_A'):
             # initialize A the same way as the default for nn.Linear and B to zero
             nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
@@ -276,23 +282,29 @@ class ConvLoRA(nn.Module, LoRALayer):
             if self.merge_weights and self.merged:
                 if self.r > 0:
                     # Make sure that the weights are not merged
-                    self.conv.weight.data -= (self.lora_B @ self.lora_A).view(self.conv.weight.shape) * self.scaling
+                    self.weight.data -= (self.lora_B @ self.lora_A).view(self.weight.shape) * self.scaling
+                    #self.conv.weight.data -= (self.lora_B @ self.lora_A).view(self.conv.weight.shape) * self.scaling
                 self.merged = False
         else:
             if self.merge_weights and not self.merged:
                 if self.r > 0:
                     # Merge the weights and mark it
-                    self.conv.weight.data += (self.lora_B @ self.lora_A).view(self.conv.weight.shape) * self.scaling
+                    self.weight.data += (self.lora_B @ self.lora_A).view(self.weight.shape) * self.scaling
+                    #self.conv.weight.data += (self.lora_B @ self.lora_A).view(self.conv.weight.shape) * self.scaling
                 self.merged = True
 
     def forward(self, x):
         if self.r > 0 and not self.merged:
-            return self.conv._conv_forward(
+            #return self.conv._conv_forward(
+            return self._conv_forward(
                 x, 
-                self.conv.weight + (self.lora_B @ self.lora_A).view(self.conv.weight.shape) * self.scaling,
-                self.conv.bias
+                self.weight + (self.lora_B @ self.lora_A).view(self.weight.shape) * self.scaling, 
+                #self.conv.weight + (self.lora_B @ self.lora_A).view(self.conv.weight.shape) * self.scaling,
+                self.bias
+                #self.conv.bias
             )
-        return self.conv(x)
+        return self._conv_forward(x)
+        #return self.conv(x)
 
 class Conv2d(ConvLoRA):
     def __init__(self, *args, **kwargs):
